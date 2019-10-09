fedacee55d04346cbfdcbdbf49b7287b817ebee1
grantxc5 <grantxc5@CIS-RDS1>
2019-10-09 15:01:47 -0500

    updated to remove nodes from a tree.

--- Ksu.Cis300.NameLookup/Dictionary.cs
+++ Ksu.Cis300.NameLookup/Dictionary.cs
@@ -1,5 +1,6 @@
 ﻿/* Dictionary.cs
  * Author: Rod Howell
+ * modified by Grant Clothier 10.09.2019
  */
 using System;
 using System.Collections.Generic;
@@ -137,6 +138,94 @@ namespace Ksu.Cis300.NameLookup
         {
             CheckKey(k);
             _elements = Add(_elements, k, v);
+        }
+
+
+        /// <summary>
+        /// returns the minimum tree from a tree that is passed in.
+        /// </summary>
+        /// <param name="t">tree that is passed in </param>
+        /// <param name="min">out parameter to be set(tree)</param>
+        /// <returns></returns>
+        private static BinaryTreeNode<KeyValuePair<TKey, TValue>> RemoveMininumKey(BinaryTreeNode<KeyValuePair<TKey, TValue>> t, out KeyValuePair<TKey, TValue> min)
+        {
+            if (t.LeftChild!=null)//if the tree has a left
+            {
+                BinaryTreeNode<KeyValuePair<TKey, TValue>> lefty =RemoveMininumKey(t.LeftChild, out min);//left child
+                BinaryTreeNode<KeyValuePair<TKey, TValue>> exit = new BinaryTreeNode<KeyValuePair<TKey, TValue>>(t.Data, lefty, t.RightChild);
+                return exit;
+            }
+            else//we are the min
+            {
+                min = t.Data;
+                return t.RightChild;
+            }
+        }
+
+        /// <summary>
+        /// retruns the result of removing a binary tree(as a binary tree)
+        /// </summary>
+        /// <param name="key">key that is passed in</param>
+        /// <param name="t">tree that is passed in</param>
+        /// <param name="removed">tree that is going to be removed</param>
+        /// <returns></returns>
+        private static BinaryTreeNode<KeyValuePair<TKey, TValue>> Remove(TKey key, BinaryTreeNode<KeyValuePair<TKey, TValue>> t, out bool removed)
+        {
+            if(t==null)
+            {
+                removed = false;
+                return t;
+            }
+            else if(t.Data.Key.CompareTo(key)<0)//trees data is less than the key
+            {
+                BinaryTreeNode<KeyValuePair<TKey, TValue>> thing = Remove(key, t.RightChild,out removed);
+                BinaryTreeNode<KeyValuePair<TKey, TValue>> exit = new BinaryTreeNode<KeyValuePair<TKey, TValue>>(t.Data, t.LeftChild, thing);
+                return exit;
+            }
+            else if(t.Data.Key.CompareTo(key) > 0)
+            {
+                BinaryTreeNode<KeyValuePair<TKey, TValue>> leftThing = Remove(key, t.LeftChild, out removed);
+                BinaryTreeNode<KeyValuePair<TKey, TValue>> exit = new BinaryTreeNode<KeyValuePair<TKey, TValue>>(t.Data, leftThing, t.RightChild);
+                return exit;
+            }
+            else
+            {
+                removed = true;
+                
+                if(t.RightChild==null)//i fliped this case with the last one
+                {
+                    return t.LeftChild;//retrun that child
+                }
+                else if(t.LeftChild == null)
+                {
+                    return t.RightChild;//retrun that child
+                }
+                else
+                {
+                    KeyValuePair<TKey, TValue> tempKey = new KeyValuePair<TKey, TValue>();
+                    BinaryTreeNode <KeyValuePair<TKey, TValue>> updateRight = RemoveMininumKey(t.RightChild, out tempKey);
+
+                    //buld a new tree min(data) as head original tree and right child
+                    BinaryTreeNode<KeyValuePair<TKey, TValue>> exit = new BinaryTreeNode<KeyValuePair<TKey, TValue>>(tempKey, t.LeftChild, updateRight);
+                    return exit;//should I create a tree with the parameters found above?
+                }
+            }
+        }
+
+        /// <summary>
+        /// removes a key and its value from the dictionary
+        /// </summary>
+        /// <param name="k">key to be removed</param>
+        /// <returns></returns>
+        public bool Remove(TKey k)
+        {
+            CheckKey(k);
+            bool a;
+            BinaryTreeNode<KeyValuePair<TKey, TValue>> thing = Remove(k,_elements,out a);
+            _elements = thing;
+            return a;
+
+
         }
     }
 }
